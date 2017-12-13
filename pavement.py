a0243653de18bb551edc05ae3432d5bedd3bcc92
tkersten09 <34385640+tkersten09@users.noreply.github.com>
2017-12-13 22:20:14 +0100

    Adjust the .gitignore file
    - setup.py will not be ignored anymore
    - the .egg-info folder should be ignored now

--- pavement.py
+++ pavement.py
@@ -3,10 +3,11 @@ import paver.doctools
 from paver.setuputils import setup
 from pathlib import Path
 
-
+pdata = paver.setuputils.find_package_data(package='pickit_cl')
+print(pdata)
 setup(
     name="pickit-cl",
-    packages=['pickit-cl/',],
+    packages=['pickit_cl/'],
     version="0.1.0",
     author="Thomas Kersten",
     author_email="tkersten09@gmail.com",
@@ -17,10 +18,12 @@ setup(
             'pickit-cl = pickit_cl.pickit_cl:run'
         ],
     },
+    package_data=pdata,
+    zip_safe=False,
 )
 
 @task
-@needs('generate_setup', 'minilib')
+@needs('generate_setup', 'minilib', 'setuptools.command.sdist')
 def makeSetup():
     """Overrides sdist to make sure that our setup.py is generated."""
     #eggdir = Path('THz.egg-info')
--- pickit_cl/lib/__init__.py
+++
@@ -1,2 +0,0 @@
-VERSION = "0.1.0"
-from .pickit_cl_ori_py3 import *
